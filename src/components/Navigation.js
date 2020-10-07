7fe081809ac0b792d78508c7444d129028bda8ca
Kim yerim <kimyerim1935@naver.com>
2020-10-07 17:59:10 +0900

    메뉴바 고정하기

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navigation.css';
-import '../image/navHam.svg';
+
+
 
 function Navigation() {
     return(
--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import './Main.css';
+import me from '../SVG/ME1.svg';
 
 function Main () {
-    return <span>Main this page </span>;
+    return (
+        <div className="main__container">
+     
+             <div className="intro__box">
+               안녕하세요 <p />
+              디자인과 영상 제작에 관심이 많은
+                 <p/>개발자 김예림 입니다.
+                 <me / >
+            </div> 
+            <div className="gra">
+                hello
+            </div>
+        </div>
+    
+    );
 }
 
 export default Main;
