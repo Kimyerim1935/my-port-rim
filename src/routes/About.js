8b02f59d530431cd077c49710321fe132dc3f04d
Kim yerim <kimyerim1935@naver.com>
2020-10-09 01:15:55 +0900

    어바웃 페이지 구조 잡기

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import './About.css';
+import Myimg2 from '../SVG/ME2.svg';
 
 function About(){
     return (
         <div className="about__container">
-            
+          <img src={Myimg2} alt="me2" className="me2" / >
+            <div className="about01">
+                <div className="textbox">
+                    <h2 className="title">Information</h2>
+                </div>   
+         </div>
         </div>
+
     );
 }
+
 
 export default About;
