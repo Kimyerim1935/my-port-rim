14eeee23955ef528c90088a467f34bcd9d6c5cd6
Kim yerim <kimyerim1935@naver.com>
2020-10-10 10:57:55 +0900

    컨택 페이지 resume 삽입

--- src/routes/Contect.js
+++ src/routes/Contect.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './Contect.css';
+import Resume from '../SVG/Resume.svg';
 
 function Contect(){
     return (
-        <div className="Contect__container">
-            hello this page is Contect
+
+    
+         <div className="Contect__container1">
+            <h2 className="contecttext">Resume</h2>
+            <img src={ Resume } alt="resume" / >
         </div>
+ 
     );
 }
 
