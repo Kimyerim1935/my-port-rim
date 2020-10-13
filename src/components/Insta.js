d2bee33774c537059106396bf65392e6edd18f92
Kim yerim <kimyerim1935@naver.com>
2020-10-13 15:08:41 +0900

    자기소개페이지1 넣기

--- src/components/Insta.js
+++ src/components/Insta.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './Textbox.css';
 import Insta from'../SVG/Insta.svg';
 import { Link } from 'react-router-dom';
-import '../routes/Instagram.css';
+import '../routes/Instagram.js';
 
 function insta (){
 return(
     <div className="contentsbox3">
         <img src={Insta} alt="Insta" className="instalogo" />
         <div className="instatext"> 
-        <Link to = "/Instagram" >Click to see more</Link>
+        <Link to="../routes/Instagram" >Click to see more</Link>
         </div>
     </div>
 );
--- src/routes/Contect.js
+++ src/routes/Contect.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import './Contect.css';
 import Resume from '../SVG/Resume.svg';
+import Intro1 from '../SVG/Intro.svg';
 
 function Contect(){
     return (
 
-    
+    <div className="Contectbody">
          <div className="Contect__container1">
             <h2 className="contecttext">Resume</h2>
             <img src={ Resume } alt="resume" className="Resume" / >
         </div>
- 
+        <div className="Contect__container2">
+            <h2 className="contecttext">Self-introduction</h2>
+            <img src= {Intro1} alt="intro1" className="Intro1" / >
+            
+        </div>
+    </div>
     );
 }
 
