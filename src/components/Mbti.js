0f7508dd443d02195439cee18795dfb7e8abaaad
Kim yerim <kimyerim1935@naver.com>
2020-10-15 00:25:09 +0900

    컴포넌트 간격 조정

--- src/components/Mbti.js
+++ src/components/Mbti.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Textbox.css';
+import Propensiity from '../SVG/Propensity.svg';
 
 function Mbti(){
 return(
     <div className="contentsbox4">
+        <img src={Propensiity} alt="Propensity" / >
     </div>
 );
 }
--- src/routes/About.js
+++ src/routes/About.js
@@ -12,11 +12,12 @@ function About(){
         <div className="about__container">
          <img src={Myimg2} alt="me2" className="me2" / >
              <div className="about01">
-                 <h2 className="title">Information</h2>
+                <h2 className="title">Information</h2>
                  <Textbox / >
                  <h2 className="title2">Education</h2>
                 <Textbox2 / >
-             </div>
+            </div>
+            
              <div className="about02">
              <h2 className="title">Interests</h2>
              <Insta / >
