d1748a743465a6d144702fa0eb3ef310cb277a65
Kim yerim <kimyerim1935@naver.com>
2020-10-21 23:12:21 +0900

    navLink active 속성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter , Route } from 'react-router-dom'
-import Navigation from './components/Navigation';
+import { BrowserRouter , Route ,NavLink} from 'react-router-dom'
 import Main from './routes/Main';
 import About from './routes/About';
 import Portfolio from './routes/Portfolio';
@@ -11,17 +10,26 @@ import Contect from './routes/Contect';
 function App() {
   return ( 
    <BrowserRouter> 
-
-     <div>
-       <Navigation/> 
-       
+     <div className="nav">
+          <div className="navlogo">
+        <h3>hello i am logo</h3>
+          </div>
+            <div className="navmenu">
+              <NavLink exact to="/" activeClassName="active">Main</NavLink>
+              <NavLink to="/About" activeClassName="active">About</NavLink>
+              <NavLink to="/Portfolio" activeClassName="active" >Portfolio</NavLink>
+              <NavLink to="/Contect" activeClassName="active" >Contect</NavLink>
+ 
+          </div>
+        
+      </div>
          <div> 
             <Route exact path='/' component={Main} /> 
             <Route path='/About' component={About} /> 
             <Route path='/Portfolio' component={Portfolio} /> 
             <Route path='/Contect' component={Contect} /> 
          </div> 
-      </div> 
+
 
   </BrowserRouter>
   );
