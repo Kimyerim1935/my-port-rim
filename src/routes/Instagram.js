2ed5d3a94723a96691aec13e9725532019eecedf
Kim yerim <kimyerim1935@naver.com>
2020-10-23 23:14:46 +0900

    컴포넌트 위치잡기

--- src/routes/Instagram.js
+++ src/routes/Instagram.js
@@ -3,27 +3,28 @@ import './Instagram.css';
 import { Route, Link } from 'react-router-dom';
 import About from './About';
 
+
+
+
+
+
 function Instagram(){
   return (
     <div className="instaContainer">
 
        <Link to="/About" className="back">Back</Link>
         <Route path='/About' component={About} />
-
         <div className="Instacontents">
 
-          <h2 className="Instatitle">Take a Picture</h2>
         </div>
+        <div className="Instacontents">
 
-        <div className="Instacontents">
-         <h2 className="Instatitle">Eat Dessert</h2>
         </div>
-       
         <div className="Instacontents">
-         <h2 className="Instatitle"> Exhibition </h2>
+
+</div>
+        
         </div>
-       
-    </div>
   );
 }
 
