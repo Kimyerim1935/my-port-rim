90714fae39dabce3ca74194f89acdab443e8bc65
Kim yerim <kimyerim1935@naver.com>
2020-10-28 19:51:11 +0900

    텍스트 박스 코드 수정

--- src/components/Textbox2.js
+++ src/components/Textbox2.js
@@ -4,14 +4,22 @@ import './Textbox.css';
 function TextBox2 (){
     return(
         <div className="contentsbox2">
-            <div className="leftbox">
+            <div className="left2">
                 2020. 09 <p/>
                 2020. 02<p/>
                 2017. 03<p/>
                 2017. 02<p/>
                 2014. 03  
             </div>
-    
+            <div className="right2">
+                동국대 미래융합원 입학 <br/>
+                경인여자대학교 졸업 <br/>
+                경인여자대학교 입학<br/>
+                은행고등학교 졸업<br />
+                은행고등학교 입학 <br/>
+
+
+            </div>
         </div>
     );
 }
