7971040a0aa9d0a6e0d0512078ee9614c2e38858
Kim yerim <kimyerim1935@naver.com>
2020-10-31 11:59:46 +0900

    Updates

---
+++ precache-manifest.0761962e6df0e9a030091875eb7bfdbc.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da1a81d1d844a13056710968bf7aba68",
+    "url": "/my-port/index.html"
+  },
+  {
+    "revision": "a9899063d67a95fe160d",
+    "url": "/my-port/static/css/main.9f1c1072.chunk.css"
+  },
+  {
+    "revision": "5a0c671cea6e5b9cd8a2",
+    "url": "/my-port/static/js/2.d665fe27.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/my-port/static/js/2.d665fe27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9899063d67a95fe160d",
+    "url": "/my-port/static/js/main.c1ff62f4.chunk.js"
+  },
+  {
+    "revision": "647724e9d0a47660c904",
+    "url": "/my-port/static/js/runtime-main.8fbd8478.js"
+  },
+  {
+    "revision": "2414f680d3ab9e030edf2a4bcba41c20",
+    "url": "/my-port/static/media/Bookmark.2414f680.svg"
+  },
+  {
+    "revision": "0e62866a64f6b6d1b196898c2c1a2763",
+    "url": "/my-port/static/media/Color.0e62866a.svg"
+  },
+  {
+    "revision": "36834ccbae2a5d0584feabfae3ae0071",
+    "url": "/my-port/static/media/Eat01.36834ccb.jpg"
+  },
+  {
+    "revision": "bf5c923afb8ca279d247b0ca4882421f",
+    "url": "/my-port/static/media/Ex01.bf5c923a.jpg"
+  },
+  {
+    "revision": "ea86547f7341b77b949f5d00fef72f50",
+    "url": "/my-port/static/media/Hashtag1.ea86547f.svg"
+  },
+  {
+    "revision": "00dfba63a450279c632cb1a9a3c4f6c5",
+    "url": "/my-port/static/media/Hashtag2.00dfba63.svg"
+  },
+  {
+    "revision": "82affb4fb96bdd799598b86eb39114cc",
+    "url": "/my-port/static/media/Hashtag3.82affb4f.svg"
+  },
+  {
+    "revision": "d112563078925afd5f2631f24fe85840",
+    "url": "/my-port/static/media/Heart.d1125630.svg"
+  },
+  {
+    "revision": "c19e38e0f39aa380797cb985679b70f0",
+    "url": "/my-port/static/media/Insta.c19e38e0.svg"
+  },
+  {
+    "revision": "402f6f8bc69fda2ac0493ea7599bffc7",
+    "url": "/my-port/static/media/InstaButton.402f6f8b.svg"
+  },
+  {
+    "revision": "e65f45a3be504330e71a0921b1ce29fa",
+    "url": "/my-port/static/media/InstaMenu.e65f45a3.svg"
+  },
+  {
+    "revision": "405fdf672e9000c4603d1ca5d5f46872",
+    "url": "/my-port/static/media/InstaPro.405fdf67.svg"
+  },
+  {
+    "revision": "a06d5171b5294ed73a4b1485a2b583c9",
+    "url": "/my-port/static/media/Intro.a06d5171.svg"
+  },
+  {
+    "revision": "fda369fef235a03937ca7464ed3731ed",
+    "url": "/my-port/static/media/Intro2.fda369fe.svg"
+  },
+  {
+    "revision": "f346c47432a379ca23f52d7aebf1669d",
+    "url": "/my-port/static/media/Logo.f346c474.svg"
+  },
+  {
+    "revision": "f753ba6b1ae677dd0dd1094a8d09978c",
+    "url": "/my-port/static/media/ME1.f753ba6b.svg"
+  },
+  {
+    "revision": "158a6285c641116a94e3d12a1f54944c",
+    "url": "/my-port/static/media/ME2.158a6285.svg"
+  },
+  {
+    "revision": "243f6a2d3b769bdedf594593033f4871",
+    "url": "/my-port/static/media/Propensity.243f6a2d.svg"
+  },
+  {
+    "revision": "924c971dba7c61627d763869e87a90ca",
+    "url": "/my-port/static/media/Resume.924c971d.svg"
+  },
+  {
+    "revision": "61a74c78336e8d8c2215d55fb8ce20c2",
+    "url": "/my-port/static/media/Take01.61a74c78.jpg"
+  },
+  {
+    "revision": "ae49856b53da63447510f8a82513c3f6",
+    "url": "/my-port/static/media/Tree01.ae49856b.svg"
+  },
+  {
+    "revision": "20e9b071e3ccb2bcd8feb11e2a71b958",
+    "url": "/my-port/static/media/Tree02.20e9b071.svg"
+  },
+  {
+    "revision": "263211284c149ae8ee6f091bb260c1f0",
+    "url": "/my-port/static/media/Tree03.26321128.svg"
+  },
+  {
+    "revision": "fb43fa07b74c35196303705172007dc6",
+    "url": "/my-port/static/media/Tree04.fb43fa07.svg"
+  },
+  {
+    "revision": "e53481c87d3d6df7d7d76307220586b8",
+    "url": "/my-port/static/media/maingr.e53481c8.svg"
+  },
+  {
+    "revision": "1d8ee6079d838293923092b0cc352956",
+    "url": "/my-port/static/media/text.1d8ee607.svg"
+  }
+]);
